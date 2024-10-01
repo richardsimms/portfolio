499b96e0a3fd4368282c72cf43dd78f1c7f8c3ee
Richard Simms <simmsdesign@gmail.com>
2024-10-01 22:12:22 +1000

    Spacing of the image on the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -60,7 +60,7 @@ export default function About() {
             alt={"me and lily"}
             width={220}
             height={260}
-            className="pointer-events-none absolute inset-0 -top-48 left-[55%] w-48 rotate-6 rounded-xl bg-gray-400 object-cover shadow-md md:left-[60%] md:w-56"
+            className="pointer-events-none absolute inset-0 -top-48 left-[45%] w-48 rotate-6 rounded-xl bg-gray-400 object-cover shadow-md md:left-[60%] md:w-56"
             priority
 
           />
